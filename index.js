cb1f5e411761882c12b05f60f6707b6fad19b7a8
vikasz1 <mauryvikas12345@gmail.com>
2023-08-28 15:15:41 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,76 @@
-const btn = document.getElementById("add-button")
-const inputText = document.getElementById("input-field")
-btn.addEventListener('click',(e)=>{
-    let inputValue=inputText.value 
-    console.log(inputValue);
-})
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://playground-vikasz1-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingItem = ref(database, "tasks");
+
+onValue(shoppingItem, function (snapshot) {
+  if (snapshot.exists()){
+
+  let tasksArray = Object.entries(snapshot.val());
+  clearShoppingList();
+  
+  for (let i = 0; i < tasksArray.length; i++) {
+    let currentItem = tasksArray[i];
+    let currentId = currentItem[0];
+    let currentItemValue = currentItem[1];
+    addItemToList(shoppingList, currentItem);
+  }
+  console.log(tasksArray);
+}
+else{
+  clearShoppingList()
+  addItemToList(shoppingItem,["item","No Items Left"])
+}
+});
+
+// console.log(database);
+
+const btn = document.getElementById("add-button");
+const inputText = document.getElementById("input-field");
+const shoppingList = document.getElementById("shopping-list");
+
+function clearInputField(field) {
+  field.value = "";
+}
+
+function addItemToList(list, item) {
+  let itemId = item[0];
+  let itemValue = item[1];
+
+  let newElement = document.createElement("li");
+  newElement.textContent = itemValue;
+
+  newElement.addEventListener("click", () => {
+    let taskToDelete = ref(database, `tasks/${itemId}`);
+    remove(taskToDelete);
+    // console.log(itemId);
+  });
+
+  shoppingList.append(newElement);
+}
+
+function clearShoppingList() {
+  shoppingList.innerHTML = "";
+}
+
+btn.addEventListener("click", (e) => {
+  let inputValue = inputText.value;
+  console.log(inputValue);
+  if (inputValue) {
+    clearInputField(inputText);
+    push(shoppingItem, inputValue);
+  }
+});
---
+++ practice.js
@@ -0,0 +1,12 @@
+let users = {
+    "00":"vikasmaury225@gmail.com",
+    "01":"mauryvikas123@gmai.com",
+    "02":"mauryavikas75@gmail.com"
+}
+
+let usersEmails = Object.values(users)
+console.log(usersEmails)
+let usersId = Object.keys(users)
+console.log(usersId);
+let usersArray = Object.entries(users)
+console.log(usersArray);
